38ea71a323d7be9ac0e634f528b12edd8693004f
tyson-tyler <125717208+tyson-tyler@users.noreply.github.com>
2025-04-19 09:46:26 +0530

    asdfa

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from "react";
 import { redirect } from "next/navigation";
 
 import { isAuthenticated } from "@/lib/actions/auth.action";
+import Logout from "@/components/ui/logout";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
@@ -11,11 +12,12 @@ const Layout = async ({ children }: { children: ReactNode }) => {
 
   return (
     <div className="root-layout">
-      <nav>
+      <nav className="flex justify-between">
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
           <h2 className="text-primary-100">InterBuddy</h2>
         </Link>
+        <Logout />
       </nav>
 
       {children}
